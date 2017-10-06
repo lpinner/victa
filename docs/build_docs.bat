883b72afac5e12a6021df9a233f306fc9cb308da
lpinner <lpinner@users.noreply.github.com>
2017-10-06 03:09:37 +0000

    Make the doc buildable from anywhere

--- docs/build_docs.bat
+++ docs/build_docs.bat
@@ -1,1 +1,8 @@
-rmdir /q /s _build _source && sphinx-apidoc -f -o _source ..\victa && make html
+@echo off
+setlocal 
+
+pushd %~d0
+
+IF EXIST _build\NUL rmdir /q /s _build _source 
+sphinx-apidoc -f -o _source ..\victa
+make html
