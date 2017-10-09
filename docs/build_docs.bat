51dbc008f57f827c9c4161ab181339b7a0cdfc83
lpinner <lpinner@users.noreply.github.com>
2017-10-09 03:16:47 +0000

    Doc tweaks and tidy up

--- docs/build_docs.bat
+++ docs/build_docs.bat
@@ -3,8 +3,10 @@ setlocal
 
 pushd %~dp0
 
-IF EXIST _build\NUL rmdir /q /s _build 
+IF EXIST _build\NUL rmdir /q /s doctrees 
+IF EXIST _source\NUL rmdir /q /s apidoc 
 IF EXIST _source\NUL rmdir /q /s _source 
+IF EXIST _source\NUL rmdir /q /s ../../docs/apidoc
 sphinx-apidoc -f -o apidoc ..\victa
 call make html
 
@@ -12,6 +14,6 @@ rem TIMEOUT /T 1 /NOBREAK
 rmdir /q /s doctrees
 rmdir /q /s apidoc
 ren html apidoc
-move apidoc ../../apidoc
+move apidoc ../../docs/apidoc
 
 :EXIT
