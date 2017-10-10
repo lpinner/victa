9b60e6d9cd47831adea2256a49bf2a911bb9d2ed
lpinner <lpinner@users.noreply.github.com>
2017-10-10 00:32:52 +0000

    doc fix

--- docs/make.bat
+++ docs/make.bat
@@ -7,7 +7,8 @@ set BUILDDIR=../../apidoc
 set SPHINXPROJ=VICTA
 set PROJDIR=..\victa
 
-sphinx-apidoc -f -o %BUILDDIR% %PROJDIR%
+sphinx-apidoc -f -o %SOURCEDIR% %PROJDIR%
+rem sphinx-build -M html %SOURCEDIR% %BUILDDIR%
 sphinx-build -M html %SOURCEDIR% %BUILDDIR%
 
 :end
